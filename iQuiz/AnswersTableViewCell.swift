2dab9593478f018b30a8a7af4beca461798ec189
labram13 <labram@uw.edu>
2024-05-04 22:13:56 -0700

    finished quiz progression

---
+++ iQuiz/AnswersTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AnswersTableViewCell.swift
+//  iQuiz
+//
+//  Created by Michaelangelo Labrador on 5/4/24.
+//
+
+import UIKit
+
+class AnswersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
